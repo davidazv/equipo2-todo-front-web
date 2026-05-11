911147fd954d38670ac47823f146a09473de8b75
davidazv <a01665896@tec.mx>
2026-05-11 00:29:34 -0600

    chore: barrel export de páginas

---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+export { default as Home } from './Home/Home'
+export { default as About } from './About/About'
+export { default as Search } from './Search/Search'
+export { default as Tasks } from './Tasks/Tasks'
+export { default as Login } from './Login/Login'
+export { default as Register } from './Register/Register'
